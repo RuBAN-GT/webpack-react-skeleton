b493438862d5867a2b039456faf63cce7262edcf
Dmitry Ruban <dkruban@gmail.com>
2017-04-22 13:24:59 +0300

    Remove junk from App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,13 +4,12 @@ import { NavLink } from 'react-router-dom';
 class App extends React.Component {
   render() {
     return (
-      <div className="main wrapper">
-        <div className="main sidebar">
-          <NavLink to="/">Root</NavLink>
-          <NavLink to="/about">About</NavLink>
+      <div className="wrapper">
+        <div className="sidebar">
+          <NavLink to="/">Home</NavLink>
         </div>
 
-        <div className="main content">
+        <div className="content">
           {this.props.children}
         </div>
       </div>
